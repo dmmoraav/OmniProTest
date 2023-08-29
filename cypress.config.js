3954ae0020d9076333eecd4b41376e433f73bd96
Air26 <air26@Admins-MacBook-Air-17.local>
2023-08-29 10:16:53 -0500

    V1.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  experimentalStudio: true,
+  screenshotOnRunFailure: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: "cypress/report",
+    charts: true,
+    reportPageTitle: 'Diego Mora Report Omni.pro Test',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
